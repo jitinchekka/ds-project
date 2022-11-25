863b6d3d1324f5a885a7b87b6f63dc25ad5c9c06
rfm-jitin <jitinchekka2@gmail.com>
2022-11-25 13:59:03 +0530

    added pd-profiling

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask import request
 from flask import render_template
 import pandas as pd
 import matplotlib.pyplot as plt
+import pandas_profiling as pp
 app = Flask(__name__)
 
 @app.route('/')
@@ -15,12 +16,18 @@ def success():
 		f=request.files['file']
 		f.save(f.filename)
 		df=pd.read_csv(f.filename)
+		profile=pp.ProfileReport(df)
+		profile.to_file(output_file="output.html")
+		
 		describe = df.describe()
 		describe.round(2)
 		print("Describe\n",describe)
 		stat = describe.head().index.values
 		return render_template("success.html",columns=df.columns,n=len(df.columns),describe=describe,stat=stat)
 		# return render_template("success.html",name=f.filename)
+	# If user does not select file, show an error message
+	else:
+		return render_template("upload.html")
 if __name__ == "__main__":
 	
 	app.run(host="127.0.0.1", port=8080, debug=True)
