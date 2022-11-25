d94ed49d111ff11bcd5752a54e8785a4c7199bf4
rfm-jitin <jitinchekka2@gmail.com>
2022-11-25 16:34:26 +0530

    added js file and file handling

--- main.py
+++ main.py
@@ -4,46 +4,56 @@ from flask import render_template
 import pandas as pd
 import matplotlib.pyplot as plt
 import pandas_profiling as pp
+
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
 	return render_template("upload.html")
 
+
 # These are the extension that we are accepting to be uploaded
-app.config['ALLOWED_EXTENSIONS'] = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'csv'])
+app.config["ALLOWED_EXTENSIONS"] = set(
+	["txt", "pdf", "png", "jpg", "jpeg", "gif", "csv"]
+)
+
 
 def allowed_file(filename):
-	return '.' in filename and \
-		   filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+	return (
+		"." in filename
+		and filename.rsplit(".", 1)[1].lower() in app.config["ALLOWED_EXTENSIONS"]
+	)
 
-@app.route('/success',methods=["POST"])
+
+@app.route("/success", methods=["POST"])
 def success():
-	if request.method=='POST':
+	if request.method == "POST":
 		# check if the post request has the file part
-		if 'file' not in request.files:
-			flash('No file part')
+		if "file" not in request.files:
+			flash("No file part")
 			return redirect(request.url)
-		f=request.files['file']
+		f = request.files["file"]
 		f.save(f.filename)
 		# If the file is not one of the allowed types, we return an error
 		if not allowed_file(f.filename):
 			return "File type is not allowed", 400
-		df=pd.read_csv(f.filename)
-		# profile=pp.ProfileReport(df)
+		df = pd.read_csv(f.filename)
+		profile=pp.ProfileReport(df)
 		# Save the report as an HTML file in templates folder
-		# profile.to_file(output_file="templates/profile.html")
+		profile.to_file(output_file="templates/profile.html")
 
 		# Add <script> tag to the HTML file to load the JS file
-		with open("templates/profile.html", "rb+") as f:
-			lines = f.readlines()
-			# Add <script> tag to the HTML file at the end to load the JS file
-			lines.insert(-1, "<script src='/static/profile.js'></script>")	
+		fd=open("templates/profile.html","a")
+		fd.write("<script src='/static/profile.js'></script>")
+		fd.close()
 		return render_template("profile.html")
 		# return render_template("success.html",name=f.filename)
 	# If user does not select file, show an error message
 	else:
 		return render_template("upload.html")
+
+
 if __name__ == "__main__":
-	
+
 	app.run(host="127.0.0.1", port=8080, debug=True)
