aca26f5bf0a27049f310a815efbb74047437e327
rfm-jitin <jitinchekka2@gmail.com>
2022-11-25 15:12:06 +0530

    profiling

--- main.py
+++ main.py
@@ -10,20 +10,36 @@ app = Flask(__name__)
 def index():
 	return render_template("upload.html")
 
+# These are the extension that we are accepting to be uploaded
+app.config['ALLOWED_EXTENSIONS'] = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'csv'])
+
+def allowed_file(filename):
+	return '.' in filename and \
+		   filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
 @app.route('/success',methods=["POST"])
 def success():
 	if request.method=='POST':
+		# check if the post request has the file part
+		if 'file' not in request.files:
+			flash('No file part')
+			return redirect(request.url)
 		f=request.files['file']
 		f.save(f.filename)
+		# If the file is not one of the allowed types, we return an error
+		if not allowed_file(f.filename):
+			return "File type is not allowed", 400
 		df=pd.read_csv(f.filename)
-		profile=pp.ProfileReport(df)
+		# profile=pp.ProfileReport(df)
 		# Save the report as an HTML file in templates folder
-		profile.to_file(output_file="templates/profile.html")
-		describe = df.describe()
-		describe.round(2)
-		print("Describe\n",describe)
-		stat = describe.head().index.values
-		return render_template("profile.html",columns=df.columns,n=len(df.columns),describe=describe,stat=stat)
+		# profile.to_file(output_file="templates/profile.html")
+
+		# Add <script> tag to the HTML file to load the JS file
+		with open("templates/profile.html", "rb+") as f:
+			lines = f.readlines()
+			# Add <script> tag to the HTML file at the end to load the JS file
+			lines.insert(-1, "<script src='/static/profile.js'></script>")	
+		return render_template("profile.html")
 		# return render_template("success.html",name=f.filename)
 	# If user does not select file, show an error message
 	else:
