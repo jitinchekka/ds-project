4bc6eed7ae09f8eeef492d17f1c37ba6010261ff
rfm-jitin <jitinchekka2@gmail.com>
2022-11-26 09:06:45 +0530

    reg python

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ from flask import Flask
 from flask import request
 from flask import render_template
 import pandas as pd
-import matplotlib.pyplot as plt
+import matplotlib
+
+matplotlib.use("Agg")
 import pandas_profiling as pp
+from sklearn.cluster import KMeans
+from sklearn import preprocessing
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error, r2_score
 
 app = Flask(__name__)
 
@@ -26,6 +32,9 @@ def allowed_file(filename):
 	)
 
 
+file_name = "diabetes.csv"
+
+
 @app.route("/success", methods=["POST"])
 def success():
 	if request.method == "POST":
@@ -34,17 +43,18 @@ def success():
 			flash("No file part")
 			return redirect(request.url)
 		f = request.files["file"]
+		file_name = f.filename
 		f.save(f.filename)
 		# If the file is not one of the allowed types, we return an error
 		if not allowed_file(f.filename):
 			return "File type is not allowed", 400
 		df = pd.read_csv(f.filename)
-		profile=pp.ProfileReport(df)
+		profile = pp.ProfileReport(df)
 		# Save the report as an HTML file in templates folder
 		profile.to_file(output_file="templates/profile.html")
 
 		# Add <script> tag to the HTML file to load the JS file
-		fd=open("templates/profile.html","a")
+		fd = open("templates/profile.html", "a")
 		fd.write("<script src='/static/profile.js'></script>")
 		fd.close()
 		return render_template("profile.html")
@@ -53,10 +63,92 @@ def success():
 	else:
 		return render_template("upload.html")
 
-@app.route("/kmeans")
-def kmeans():
-	return render_template("kmeans.html")
 
+@app.route("/clustering_before")
+def kmeans():
+	f = file_name
+	df = pd.read_csv(f)
+	return render_template("clustering.html", columns=df.columns.values)
+
+
+@app.route("/clustering_after", methods=["POST"])
+def clustering():
+	# Print the values of the form
+	print(request.form)
+	if request.method == "POST":
+		f = file_name
+		i = request.form["x"]
+		j = request.form["y"]
+		n = int(request.form["x_value"])
+		print("n=", n)
+		df = pd.read_csv("diabetes.csv")
+		print(i, j)
+		# Sklearn K means clustering
+		kmeans = KMeans(n_clusters=n).fit(df[[i, j]])
+		# Plot the clusters with cluster centers
+		matplotlib.pyplot.scatter(
+			df[i], df[j], c=kmeans.labels_.astype(float), s=50, alpha=0.5
+		)
+		matplotlib.pyplot.xlabel(i)
+		matplotlib.pyplot.ylabel(j)
+		matplotlib.pyplot.title("K means clustering")
+		# Save the plot as a png file in static folder
+		matplotlib.pyplot.savefig("static/{}_{}.png".format(i, j))
+		# Return the plot to the user
+		graph = "{}_{}.png".format(i, j)
+		print("Graph is ", graph)
+		# graphs.append(fig)
+		print(df.columns)
+	return render_template("rako_k_means.html", columns=df.columns, graph=graph)
+	# return render_template("kmeans.html",columns=df.columns)
+
+
+@app.route("/prediction_before")
+def prediction():
+	f = file_name
+	df = pd.read_csv(f)
+	return render_template("prediction_before.html", columns=df.columns.values)
+
+@app.route("/prediction_after", methods=["POST"])
+def prediction_after():
+	print("POST request",request.form)
+	x_column = request.form["x"]
+	y_column = request.form["y"]
+	x_value = float(request.form["x_value"])
+	df = pd.read_csv(file_name)
+	# Split the data into training/testing sets
+	diabetes_X_train = df[x_column][:-20]
+	diabetes_X_test = df[x_column][-20:]
+	diabetes_y_train = df[y_column][:-20]
+	diabetes_y_test = df[y_column][-20:]
+
+	# Create linear regression object
+	regr = linear_model.LinearRegression()
+	# Train the model using the training sets
+	regr.fit(diabetes_X_train.values.reshape(-1, 1), diabetes_y_train)
+	# Make predictions using the testing set
+	diabetes_y_pred = regr.predict(diabetes_X_test.values.reshape(-1, 1))
+
+	# The coefficients
+	print("Coefficients: ", regr.coef_)
+	# The mean squared error
+	print("Mean squared error: %.2f" % mean_squared_error(diabetes_y_test, diabetes_y_pred))
+	predicted_value = regr.predict([[x_value]])
+	print("Predicted value: ", predicted_value)
+	# The coefficient of determination: 1 is perfect prediction
+	print("Coefficient of determination: %.2f" % r2_score(diabetes_y_test, diabetes_y_pred))
+
+	# Plot output
+	matplotlib.pyplot.scatter(diabetes_X_test, diabetes_y_test, color="black")
+	matplotlib.pyplot.plot(diabetes_X_test, diabetes_y_pred, color="blue", linewidth=3)
+	matplotlib.pyplot.xlabel(x_column)
+	matplotlib.pyplot.ylabel(y_column)
+	matplotlib.pyplot.title("Linear Regression")
+	matplotlib.pyplot.savefig("static/{}_{}.png".format(x_column, y_column))
+	# clear the plot
+	matplotlib.pyplot.clf()
+	graph = "{}_{}.png".format(x_column, y_column)
+	return render_template("prediction_after.html", graph=graph, predicted_value=predicted_value.item(),columns=df.columns)
 if __name__ == "__main__":
 
 	app.run(host="127.0.0.1", port=8080, debug=True)
